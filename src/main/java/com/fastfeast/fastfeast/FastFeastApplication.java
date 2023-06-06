ce9d477fc7f7eaf70107f265ef365aaaeac34f16
Kris Poole <krispoole@pm.me>
2023-06-06 15:05:43 -0500

    initial commit

---
+++ src/main/java/com/fastfeast/fastfeast/FastFeastApplication.java
@@ -0,0 +1,13 @@
+package com.fastfeast.fastfeast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFeastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFeastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastfeast/fastfeast/FastFeastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfeast.fastfeast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFeastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
